8c6071a73ad08dc4f7771cc6853ad229f86e12a8
sudipwisely <contact2sudipsen@gmail.com>
2023-06-19 23:19:10 +0530

    3rd Core commit

--- src/main.py
+++ src/main.py
@@ -5,3 +5,15 @@ app = FastAPI()
 @app.get('/')
 def index():
     return {'data':{'name':'sudip'}}
+
+@app.get('/about')
+def about():
+    return {'data':{'pagename':'about'}}
+
+@app.get('/course')
+def course():
+    return {'data':'all courses'}
+
+@app.get('/course/{id}')
+def individualcourse(id: int):
+    return {'course_id':id}
