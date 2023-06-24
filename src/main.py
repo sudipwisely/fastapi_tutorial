1a316464f8b0013860855c45bf1cb6afb5659838
sudipwisely <contact2sudipsen@gmail.com>
2023-06-24 15:23:43 +0530

    main.py code change

--- src/main.py
+++ src/main.py
@@ -5,15 +5,3 @@ app = FastAPI()
 @app.get('/')
 def index():
     return {'data':{'name':'sudip'}}
-
-@app.get('/about')
-def about():
-    return {'data':{'pagename':'about'}}
-
-@app.get('/course')
-def course():
-    return {'data':'all courses'}
-
-@app.get('/course/{id}')
-def individualcourse(id: int):
-    return {'course_id':id}
