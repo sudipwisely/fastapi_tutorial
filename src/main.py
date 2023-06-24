7e5998429db851452d1e5fd468f6b5a9579d7429
sudipwisely <contact2sudipsen@gmail.com>
2023-06-24 15:38:01 +0530

    main.py2

--- src/main.py
+++ src/main.py
@@ -5,3 +5,19 @@ app = FastAPI()
 @app.get('/')
 def index():
     return {'data':{'name':'sudip'}}
+
+@app.get('/contact')
+def contact():
+    return {'data':{'page':'this is contact page'}}
+
+@app.get('/contact')
+def contact():
+    return {'data':{'page':'this is contact page'}}
+
+@app.get('/all_courses')
+def all_courses():
+    return {'data':{'page':'all courses list'}}
+
+@app.get('/course/{id}')
+def single_course(id:int):
+    return {'courseID':id}
